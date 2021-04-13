66d496d1fd5d2cb57609d3fa8a0453cb0e5e6c68
Wenpin Hou <wp.hou3@gmail.com>
2021-04-13 18:35:47 -0400

    add path1: gene fdr, curve dot.quantiile

--- pseudotime_lung/code/05_downstream_analysis.R
+++ pseudotime_lung/code/05_downstream_analysis.R
@@ -1,9 +1,9 @@
 rm(list = ls())
 library(here)
-# setwd(here())
-# source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
-setwd('/Users/wenpinhou/Dropbox/pardoll/nsclc/')
-source('/Users/wenpinhou/Dropbox/trajectory_variability/function/01_function.R')
+setwd(here())
+source('/home-4/whou10@jhu.edu/scratch/Wenpin/trajectory_variability/function/01_function.R')
+# setwd('/Users/wenpinhou/Dropbox/pardoll/nsclc/')
+# source('/Users/wenpinhou/Dropbox/trajectory_variability/function/01_function.R')
 for (path in c('path1', 'path2', 'path3')){
  #path = 'path2'
   rdir <- paste0('pseudotime_lung/result/', path, '/')
@@ -14,8 +14,9 @@ for (path in c('path1', 'path2', 'path3')){
   # downstream analysis pipeline #  
   # ---------------------------- #
   Res <- readRDS(paste0(rdir, '/testtime_res.rds'))
-  
-  diffgene = rownames(Res$statistics)[Res$statistics[,grep('^fdr.*overall$', colnames(Res$statistics))] < 0.05]
+  s = Res$statistics
+  s = s[order(s[,1], -s[,3]),]
+  diffgene = rownames(s)[s[,1] < 0.05]
   str(diffgene)
   ## --------------
   ## population fit
@@ -76,16 +77,53 @@ for (path in c('path1', 'path2', 'path3')){
   print(plotFitHm(Res, showRowName = T, subsampleCell = F, cellWidthTotal = 300, cellHeightTotal = length(Res$cluster) * 10))
   dev.off()
   
-  gene <- c('GZMA', 'CCL5', 'NKG7', 'GZMK', 'IL6R', 'SELL', 'CD74', 'CCR7', 'IFNGR2', 'TCF7', 'HLA-DPA1', 'IL2RA', 'EOMES', 'IFNG', 'GZMB')
+  # gene <- c('GZMA', 'CCL5', 'NKG7', 'GZMK', 'IL6R', 'SELL', 'CD74', 'CCR7', 'IFNGR2', 'TCF7', 'HLA-DPA1', 'IL2RA', 'EOMES', 'IFNG', 'GZMB')
+  Res = readRDS(paste0(pdir, 'testtime_res_with_clu.rds'))
+  fdr <- Res$statistics[,1]
+  names(fdr) <- rownames(Res$statistics)
+  fdr <- sapply(fdr, function(i){
+    if (i < 1e-5){
+      '<1e-5'
+    } else {
+      paste0('=',round(i, 2))
+    }
+  })
+  rownames(Res$populationFit) <- paste0(rownames(Res$populationFit), '; FDR', fdr[rownames(Res$populationFit)])
+  rownames(Res$expr) <- paste0(rownames(Res$expr), '; FDR', fdr[rownames(Res$expr)])
+  gene <- c('GZMA','NKG7', 'GZMK','HLA-DRA','PRF1','IFNG','CCR6','CCL5','ITGB2','CRTAM','RUNX3','EOMES','SELL', 'TCF7','IL7R')
+  str(gene)
+  # 
+  gene <- paste0(gene, '; FDR', fdr[gene])
+  gene <- gene[gene %in% rownames(Res$populationFit)]
+  png(paste0(pdir, 'example_genes_pullplot.png'),width = 2000,height = 1800, res = 300)
+  plotGeneCellAndPopulation(Res, gene = gene, dot.quantile = 0.99)
+  dev.off()
+  
+  
+  Res = readRDS(paste0(pdir, 'testtime_res_with_clu.rds'))
+  gene <- c('GZMA','NKG7', 'GZMK','HLA-DRA','PRF1','IFNG','CCR6','CCL5','ITGB2','CRTAM','RUNX3','EOMES','SELL', 'TCF7','IL7R')
   gene <- gene[gene %in% rownames(Res$populationFit)]
   png(paste0(pdir, 'example_genes.png'),width = 2000,height = 1800, res = 300)
   plotGene(Res, gene, plot.point = T, point.size =0.2)
   dev.off()
   
-  png(paste0(pdir, 'example_genes_pullplot.png'),width = 2000,height = 1800, res = 300)
-  plotGeneCellAndPopulation(Res, gene = gene)
-  dev.off()
+  if (path == 'path1'){
+    s = Res$statistics
+    s = s[order(s[,1], -s[,3]), ]
+    s = s[s[,1] < 0.05, ]
+    str(s)
+    dg = rownames(s)[1:30]
+    str(dg)
+    dg = unique(c(dg, gene))
+    Res$populationFit <- Res$populationFit[dg, ]
+    colnames(Res$populationFit) <- colnames(Res$expr)
+    Res$cluster <- Res$cluster[dg]
+    png(paste0(pdir, 'fitHm_with_genenames_30_and_selected.png'),width = 6000,height = 3000,res = 300)
+    plotFitHm(Res, showRowName = T, subsampleCell = F, cellWidthTotal = 300, cellHeightTotal = length(Res$cluster) * 10)
+    dev.off()
+  }
 }
   
+
 
 
